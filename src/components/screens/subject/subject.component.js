f71e7bc10357eadd4ef4be172f1f9a323cbe8b57
Ayobamiu <ayobamiu@gmail.com>
2021-03-05 08:41:11 +0100

    Better

--- src/components/screens/subject/subject.component.js
+++ src/components/screens/subject/subject.component.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import './css/style.css';
+import './css/style.css'; 
 // import { Link, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
