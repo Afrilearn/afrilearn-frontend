e6dd903206e7971dc04d4b33bba6b56b3a2b4533
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-04-26 11:18:17 +0100

    Feature: Parent signup

--- src/components/screens/signup/signup.component.js
+++ src/components/screens/signup/signup.component.js
@@ -85,7 +85,7 @@ const Signup = (props) => {
     let message;
     if (!role) {
       message = "Please select a role";
-    } else if (!courseId) {
+    } else if (role !== '606ed82e70f40e18e029165e' && !courseId) {
       message = "Please select a class";
     } else if (!fullName) {
       message = "Please enter full name";
@@ -99,7 +99,7 @@ const Signup = (props) => {
 
     if (
       !role ||
-      !courseId ||
+      (role !== '606ed82e70f40e18e029165e' && !courseId) ||
       !fullName ||
       !email ||
       !password ||
@@ -187,7 +187,7 @@ const Signup = (props) => {
                 {roleSet()}
               </select>
             </div>
-            <div className="col-md-12">
+            {role !== '606ed82e70f40e18e029165e' && <div className="col-md-12">
               <select
                 className="general"
                 name="courseId"
@@ -197,7 +197,7 @@ const Signup = (props) => {
                 <option>Select class</option>
                 {classSet()}
               </select>
-            </div>
+            </div>}
             {role === "602f3ce39b146b3201c2dc1d" ? (
               <div className="col-md-12">
                 <input
